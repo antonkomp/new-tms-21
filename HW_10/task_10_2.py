8eed622f5c71ef6dcbe5a8100a7e5844dc8c40af
antonkomp <antonkomp@gmail.com>
2020-10-19 02:37:26 +0300

    edited task 2

--- HW_10/task_10_2.py
+++ HW_10/task_10_2.py
@@ -2,6 +2,7 @@ import csv, requests
 from time import time
 from datetime import datetime
 
+DAYS = 6  # data are taken for 6 days, not 7, because you cannot get data for more than 6 days from the site for free
 api_url = 'https://api.openweathermap.org/data/2.5/onecall/timemachine'
 params = {
     'lat': '53.842761',  # width of the Minsk
@@ -11,24 +12,25 @@ params = {
     'appid': '466673d528f019d751e7973b7a9d1bb1'  # API key
 }
 fields = ['date', 'location', 'temperature', 'wind speed']
-rows_data = [[0 for x in range(4)] for j in range(6)]
+rows_data = [[] for i in range(DAYS)]
 sum_temp = 0
 sum_wind_speed = 0
-# данные берутся за 6 дней, а не за 7, т.к. с сайта бесплатно нельзя получить данные более чем за 6 дней
-for i in range(6):
+
+for i in range(DAYS):
     res = requests.get(api_url, params=params)
     data = res.json()
-    rows_data[i][0] = datetime.fromtimestamp(params['dt']).strftime('%Y-%m-%d %H:%M:%S')
-    rows_data[i][1] = 'Minsk'
-    rows_data[i][2] = data['current']['temp']
+    rows_data[i].append(datetime.fromtimestamp(params['dt']).strftime('%Y-%m-%d %H:%M:%S'))
+    rows_data[i].append('Minsk')
+    rows_data[i].append(data['current']['temp'])
     sum_temp += data['current']['temp']
-    rows_data[i][3] = data['current']['wind_speed']
+    rows_data[i].append(data['current']['wind_speed'])
     sum_wind_speed += data['current']['wind_speed']
+    # the parameter must be changed after executing the cycle, since its initial value is already set
     params['dt'] -= 86400
 
 rows_average_data = [
-    ['average temperature', round(sum_temp / 6, 2)],
-    ['average wind speed', round(sum_wind_speed / 6, 2)]
+    ['average temperature', round(sum_temp / DAYS, 2)],
+    ['average wind speed', round(sum_wind_speed / DAYS, 2)]
 ]
 
 filename = 'weather_in_Minsk.csv'
