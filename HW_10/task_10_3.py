ea16985f7b01e20c27da33722ae0009a3410174c
antonkomp <antonkomp@gmail.com>
2020-10-19 03:24:32 +0300

    edited task 3

--- HW_10/task_10_3.py
+++ HW_10/task_10_3.py
@@ -1,25 +1,20 @@
 from random import randint
 from datetime import datetime
 from time import time
-import csv
 
 date = []
 for i in range(30):
     timestamp = randint(1, int(time()))
-    data = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+    data = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d\n')
     date.append(data)
 
-filename = 'date_file.csv'
-with open(filename, 'w', newline='') as csvdata:
-    csvwriter = csv.writer(csvdata)
-    csvwriter.writerow(date)
+filename = 'date_file.txt'
+with open(filename, 'w') as data:
+    data.writelines(date)
 
-with open(filename, 'r', newline='') as file_read:
-    csvreader = csv.reader(file_read)
-    list_csv_file = next(csvreader)
-
-min_date = list_csv_file[0]
-for i in range(len(list_csv_file) - 1):
-    if min_date > list_csv_file[i + 1]:
-        min_date = list_csv_file[i + 1]
+min_date = '3'
+with open(filename, 'r') as file_read:
+    for line in file_read.readlines():
+        if min_date > line:
+            min_date = line
 print(min_date)
