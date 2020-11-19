c5cd6c2d5db9f7ab172af2c34d180b33579f8c95
antonkomp <antonkomp@gmail.com>
2020-11-19 08:54:29 +0300

    edited hw12_01

--- HW_12/task_12_1.py
+++ HW_12/task_12_1.py
@@ -1,8 +1,20 @@
 class MyTime:
-    def __init__(self, hours=0, minutes=0, seconds=0):
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
+    def __init__(self, hours=0, minutes=0, seconds=0, time_str=None, other=None):
+        if other:
+            self.hours = other.hours
+            self.minutes = other.minutes
+            self.seconds = other.seconds
+        elif time_str:
+            val = time_str.split(':')
+            self.hours = int(val[0])
+            self.minutes = int(val[1])
+            self.seconds = int(val[2])
+            self.total_seconds = self.hours * 3600 + self.minutes * 60 + self.seconds
+        else:
+            self.hours = hours
+            self.minutes = minutes
+            self.seconds = seconds
+            self.total_seconds = self.hours * 3600 + self.minutes * 60 + self.seconds
 
     # =
     def __eq__(self, other):
@@ -22,27 +34,23 @@ class MyTime:
 
     # <
     def __lt__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
         o_time = other.hours * 3600 + other.minutes * 60 + other.seconds
-        return s_time < o_time
+        return self.total_seconds < o_time
 
     # >
     def __gt__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
         o_time = other.hours * 3600 + other.minutes * 60 + other.seconds
-        return s_time > o_time
+        return self.total_seconds > o_time
 
     # <=
     def __le__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
         o_time = other.hours * 3600 + other.minutes * 60 + other.seconds
-        return s_time <= o_time
+        return self.total_seconds <= o_time
 
     # >=
     def __ge__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
         o_time = other.hours * 3600 + other.minutes * 60 + other.seconds
-        return s_time >= o_time
+        return self.total_seconds >= o_time
 
     # +
     def __add__(self, other):
@@ -55,9 +63,8 @@ class MyTime:
 
     # -
     def __sub__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
         o_time = other.hours * 3600 + other.minutes * 60 + other.seconds
-        my_time = s_time - o_time
+        my_time = self.total_seconds - o_time
         if my_time < 0:
             my_time = 86400 - abs(my_time)
             hours = my_time // 3600
@@ -71,13 +78,12 @@ class MyTime:
 
     # * on number
     def __mul__(self, other):
-        s_time = self.hours * 3600 + self.minutes * 60 + self.seconds
-        my_time = s_time * other
+        my_time = self.total_seconds * other
         hours = my_time // 3600 % 24
         minutes = my_time % 3600 // 60
         seconds = my_time % 60
         return MyTime(hours, minutes, seconds)
 
     # show time
-    def __repr__(self):
+    def __str__(self):
         return f'{self.hours}:{self.minutes}:{self.seconds}'
