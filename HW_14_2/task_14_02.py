3a01a0c8c1ba7f9075b12dd9e69710ab385f7034
antonkomp <antonkomp@gmail.com>
2020-11-24 06:13:45 +0300

    edited task 2

--- HW_14_2/task_14_02.py
+++ HW_14_2/task_14_02.py
@@ -6,11 +6,11 @@ import time
 def create_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--name', required=True)
-    parser.add_argument('-sn', '--surname', required=True)
-    parser.add_argument('-tf', '--timefocus', type=int, default=25)
-    parser.add_argument('-tb', '--timebreak', type=int, default=5)
-    parser.add_argument('-nc', '--numbercycles', type=int, default=4)
-    parser.add_argument('-tn', '--taskname', required=True)
+    parser.add_argument('-s', '--surname', required=True)
+    parser.add_argument('-f', '--timefocus', type=int, default=25)
+    parser.add_argument('-b', '--timebreak', type=int, default=5)
+    parser.add_argument('-c', '--numbercycles', type=int, default=4)
+    parser.add_argument('-t', '--taskname', required=True)
     args_func = parser.parse_args()
     return args_func
 
@@ -25,28 +25,16 @@ def add_record(name, surname, taskname):
 def pomodoro(timefocus, timebreak, numbercycles):
     for i in range(numbercycles):
         total_time_focus = timefocus * 60
-        print(f'Now {i + 1} cycle')
+        total_time_break = timebreak * 60
+        print('Focus!!!')
         while total_time_focus > 0:
             time.sleep(1)
             minutes = total_time_focus // 60
-            if minutes // 10 < 1:
-                minutes = '0' + str(minutes)
             seconds = total_time_focus % 60
-            if seconds // 10 < 1:
-                seconds = '0' + str(seconds)
-            print(f'focus time - {minutes}:{seconds}')
+            print(f'{minutes:02}:{seconds:02}')
             total_time_focus -= 1
-        total_time_break = timebreak * 60
-        while total_time_break > 0:
-            time.sleep(1)
-            minutes = total_time_break // 60
-            if minutes // 10 < 1:
-                minutes = '0' + str(minutes)
-            seconds = total_time_break % 60
-            if seconds // 10 < 1:
-                seconds = '0' + str(seconds)
-            print(f'break - {minutes}:{seconds}')
-            total_time_break -= 1
+        print('Break!!!')
+        time.sleep(total_time_break)
 
 
 if __name__ == '__main__':
